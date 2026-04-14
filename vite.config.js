e07644b5d7f3e90e0651063ab9ecb29455ba7e25
kushal639 <kushal639@github.com>
2026-04-14 14:10:40 +0000

    Initial commit: EvidenceX project

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+
+  base: "/EvidenceX/",
+
+  server: {
+    port: 5000,
+    host: '0.0.0.0',
+    allowedHosts: true,
+    proxy: {
+      '/api/cctv': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    },
+  },
+})
